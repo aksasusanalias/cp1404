0c08beacbd277a502c90e3b79b34b9c3b33034f2
aksasusanalias <aksa.susanalias@my.jcu.edu.au>
2023-06-11 18:17:51 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,18 @@
-password = input("Enter password : ")
-word_len = len(password)
-for i in range(1, word_len+1):
-    print("*", end='')
-print("\n")
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    word_len = len(password)
+    for i in range(1, word_len + 1):
+        print("*", end='')
+    print("\n")
+
+
+def get_password():
+    password = input("Enter password : ")
+    return password
+
+
+main()
