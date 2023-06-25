a6fc2338ec628cf852014615e7302bcdcee5847e
aksasusanalias <aksa.susanalias@my.jcu.edu.au>
2023-06-26 00:26:25 +1000

    Add to prac3

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,12 +1,13 @@
 import random
 
-MIN_NUMBER = 1
-MAX_NUMBER = 45
+MINIMUM_VALUE = 1
+MAXIMUM_VALUE = 45
 AMOUNT_OF_NUMBERS_PER_QUICK_PICK = 6
 
 number_of_quick_picks = int(input("How many quick picks? "))
 
 for i in range(number_of_quick_picks):
-    number = random.sample(range(MIN_NUMBER, MAX_NUMBER + 1), AMOUNT_OF_NUMBERS_PER_QUICK_PICK)
+    number = random.sample(range(MINIMUM_VALUE, MAXIMUM_VALUE + 1),
+                           AMOUNT_OF_NUMBERS_PER_QUICK_PICK)
     numbers = sorted(number)
     print("{:2} {:2} {:2} {:2} {:2} {:2}".format(*numbers))
