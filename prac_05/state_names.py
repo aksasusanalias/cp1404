192d1cc35aa64e9b1094c9d2b62cdbd110b9ab24
aksasusanalias <aksa.susanalias@my.jcu.edu.au>
2023-07-02 22:30:43 +1000

    complete program state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,6 +3,11 @@ CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
+"""
+State Names
+Estimate: 30 minutes
+Actual:   35 minutes
+"""
 
 # Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
